d1fbe034777ec642320fad95ace4b1bdcf131c1f
Jacob Allen <47609209+jallen2034@users.noreply.github.com>
2021-04-14 10:33:30 -0700

    Update input.js
    Cleaned up comments to make repo more presentable

--- input.js
+++ input.js
@@ -1,30 +1,30 @@
 // global connection variable which stores the active TCP connection object.
 let connection; 
 
-// file which will handle all of the user input
+/* handle all of the user input + connection global var gets set to the conn object which was passed in as a param, from client.js
+ * check the input on stdin, first param is the data from stdin, and second param is the callback function we pass in to 
+ * 'data' returned from server is passed in as a paramater to handleUserInput() function
+ * check if they hit ctrl + c */
 const setupInput = function(conn) {
-  // connection global var gets set to the conn object which was passed in as a param, from client.js
   connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  // here we're checking the input on stdin, first param is the data from stdin, and second param is the callback function we pass in to 
-  // 'data' returned from server is passed in as a paramater to handleUserInput() function
-  // check if they hit ctrl + c
   stdin.on('data', handleUserInput);
   return stdin;
 };
 
-// function to handle a ctrl + c exit from stdin
+/* function to handle a ctrl + c exit from stdin
+ * if check to input "ctrl + c" to escape our terminal */
 const handleUserInput = function(stdinInput) {
-  // this check allows us to input "ctrl + c" to escape our terminal
+ 
   if (stdinInput === '\u0003') {
     process.exit();
   }
   
-  // handle wasd key strokes to move snake here
-  // if the stdinInput from the user is W, update global connection variable to write out the keystroke from the user to be "piped out" to the server
+  /* handle wasd key strokes to move snake here
+   * if the stdinInput from the user is W, update global connection variable to write out the keystroke from the user to be sent to the server */
   if (stdinInput === 'w') {
     console.log("Pressed! W");
 
@@ -70,6 +70,4 @@ const handleUserInput = function(stdinInput) {
   }
 };
 
-// export setupInput() function from this module to be used elsewhere when needed
-// https://www.tutorialsteacher.com/nodejs/nodejs-module-exports
 module.exports = setupInput;
